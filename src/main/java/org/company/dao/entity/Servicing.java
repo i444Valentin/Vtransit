f84292ff071d58c174c8b3628643cee2ed2dc161
Walentin <valession@gmail.com>
2023-12-04 12:41:38 +0300

    Added Entity: Servicing

---
+++ src/main/java/org/company/dao/entity/Servicing.java
@@ -0,0 +1,40 @@
+package org.company.dao.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Check;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="servicing")
+@Data
+public class Servicing {
+    @Id
+    @Column(name="tabel_number")
+    private Integer tabel_number;
+
+    @Column(name="traveled")
+    private Integer traveled;
+
+    @Column(name="engine_state",length = 20 )
+    @Check(constraints = "engine_state::text = 'Исправен'::text " +
+            "OR engine_state::text = 'Необходимо обслужить'::text " +
+            "OR engine_state::text = 'Неисправен'::text")
+    private String engineState;
+
+    @Column(name="transmission_state",length = 20 )
+    @Check(constraints = "engine_state::text = 'Исправна'::text " +
+            "OR engine_state::text = 'Необходимо обслужить'::text " +
+            "OR engine_state::text = 'Неисправна'::text")
+    private String transmissionState;
+
+    @Column(name="common_state",length = 20 )
+    @Check(constraints = "engine_state::text = 'Исправен'::text " +
+            "OR engine_state::text = 'Необходимо обслужить'::text " +
+            "OR engine_state::text = 'Неисправен'::text")
+    private String commonState;
+
+    @Column(name="last_service")
+    private LocalDate lastService;
+}
