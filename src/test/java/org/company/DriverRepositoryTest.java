339355ea6937d5ea2c29ee9fe665017e9189f6f7
Walentin <valession@gmail.com>
2023-12-05 15:02:53 +0300

    Testing: added a test

---
+++ src/test/java/org/company/DriverRepositoryTest.java
@@ -0,0 +1,26 @@
+package org.company;
+
+import org.company.dao.entity.Driver;
+import org.company.repository.DriverRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class DriverRepositoryTest {
+
+    @Autowired
+    DriverRepository driverRepository;
+    @Test
+    public void getDriverById(){
+        Driver driver = driverRepository.getReferenceById(3);
+        assertNotNull(driver);
+        String expectedFirstName = "Иванов";
+        String actualFirstName = driver.getFirstName();
+        Assertions.assertEquals(expectedFirstName,actualFirstName);
+    }
+}
